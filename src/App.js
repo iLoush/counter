7d20f13569234d3434b8bf1337a06dab3ae52676
Irina <iloushkina@gmail.com>
2020-08-25 10:23:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CounterClass from './CounterClass'
+import CounterFunc from './CounterFunc'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Counter 1 (using class)
+      <CounterClass initialCount={0} />
+
+      Counter 2 (using function)
+      <CounterFunc initialCount={0} />
+    </>
   );
 }
 
---
+++ src/CounterClass.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class CounterClass extends Component {
+   constructor(props) {
+      super(props)
+
+      this.state = {
+         count: props.initialCount
+      }
+   }
+   render() {
+      return (
+         <div>
+            <button
+               onClick={() => this.changeCount(-1)}>
+               -
+            </button>
+            <span>{this.state.count}</span>
+            <button
+               onClick={() => this.changeCount(+1)}>
+               +
+            </button>
+         </div>
+      )
+   }
+
+   changeCount(amount) {
+      this.setState(prevState => {
+         return { count: prevState.count + amount }
+      })
+   }
+};
